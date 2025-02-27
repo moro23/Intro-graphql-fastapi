a24abcc3fba842647ce4e523465b70aedad1e2b1
moro23 <billhab66@gmail.com>
2025-02-27 17:28:33 +0000

    updated main file.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from strawberry.fastapi import GraphQLRouter 
+
+## lets create an instance of a GraphQLRouter 
+graphql_router = GraphQLRouter(..., path="/", graphql_ide="lolo-sandbox")
 
 app = FastAPI()
 
+app.include_router(graphql_router)
 
-@app.get("/")
-async def hello_world():
-    return {"message": "Hello World"}
